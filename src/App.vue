5dea8743c36d09c99708c741cc5941628d1a2683
a_kryuchenkov <a.kryuchenkov2@gmail.com>
2018-10-05 19:25:58 +0300

    sidenav animation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <sidenav />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Sidenav from '@/components/layout/Sidenav';
+import Navbar from '@/components/layout/Navbar';
+
+export default {
+  components: {
+    Sidenav,
+    Navbar
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ src/components/layout/Sidenav.vue
@@ -0,0 +1,277 @@
+<template>
+    <aside>
+        <p>{{ title }}</p>
+
+        <section>
+            <div 
+                v-for="(link, i) in links" 
+                :key="i">
+                <router-link 
+                    v-if="!link.children" 
+                    :to="link.to">
+                    {{link.name}}
+                </router-link>
+                <div
+                    v-else
+                    @click="showChildres(link)">
+                    {{link.name}}
+                    <transition name="slide">
+                        <div v-if="link.isShowChildren">
+                            <router-link 
+                                v-for="(childLink, j) in link.children"
+                                :key="j"
+                                :to="childLink.path">
+                                {{childLink.name}}
+                            </router-link>
+                        </div>
+                    </transition>
+                </div>
+            </div>
+        </section>
+    </aside>
+</template>
+
+<script>
+export default {
+  name: 'Sidenav',
+  data() {
+    return {
+      title: 'Информационная поддержка жизненного цикла',
+
+      links: [
+        {
+          name: 'Главная',
+          icon: '',
+          path: '/',
+          children: []
+        },
+        {
+          name: 'ИПИ в Нижегородской области',
+          icon: '',
+          path: '/',
+          children: []
+        },
+        {
+          name: 'Лекции',
+          icon: '',
+          path: '/',
+          children: [
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            }
+          ]
+        },
+        {
+          name: 'Тесты',
+          icon: '',
+          path: '/',
+          children: [
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            }
+          ]
+        },
+        {
+          name: 'Практика',
+          icon: '',
+          path: '/',
+          children: [
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            },
+            {
+              name: 'Лекции',
+              icon: '',
+              path: '/'
+            }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    showChildres(link) {
+      if (!link.children.length) {
+        return;
+      }
+
+      this.links = this.links.map(linkInside => {
+        linkInside.name === link.name
+          ? (linkInside.isShowChildren = !linkInside.isShowChildren)
+          : (linkInside.isShowChildren = false);
+        return linkInside;
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: margin-top 0.5s ease-out, opacity 0.5s ease;
+}
+
+.slide-enter,
+.slide-leave-to {
+  margin-top: -20px;
+  opacity: 0;
+}
+
+.slide-enter-to,
+.slide-leave {
+  opacity: 1;
+  margin-top: 0px;
+}
+</style>
